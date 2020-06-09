b5e7fc3a01972b3e7e92e72fdec5d499db3dba2f
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2020-06-08 21:21:13 -0300

    Loop through pulpcore versions

--- pdc.py
+++ pdc.py
@@ -19,26 +19,34 @@ PULP_PLUGINS = [
 
 pulpcore_url = PYPI_ROOT.format("pulpcore")
 response = requests.get(pulpcore_url)
-pulpcore_version = response.json()["info"]["version"]
-print(f"\nLastest pulpcore version: {pulpcore_version}")
-parsed = []
+pulpcore_releases = response.json()["releases"].keys()
+to_remove = []
 
-for plugin in PULP_PLUGINS:
-    pkg_url = PYPI_ROOT.format(plugin)
-    response = requests.get(pkg_url)
-    if response.status_code == 404:
-        print(f"{plugin}  not found on PyPI")
-        continue
+for pulpcore_version in reversed([*pulpcore_releases]):
+    PULP_PLUGINS = [i for i in PULP_PLUGINS if i not in to_remove]
+    shown = False
+    for plugin in PULP_PLUGINS:
+        pkg_url = PYPI_ROOT.format(plugin)
+        response = requests.get(pkg_url)
+        if response.status_code == 404:
+            print(f"{plugin}  not found on PyPI")
+            continue
 
-    plugin_version = response.json()["info"]["version"]
-    plugin_requirements = response.json()["info"]["requires_dist"]
-    pulpcore_requirement = [r for r in plugin_requirements if "pulpcore" in r][0]
-    req = pulpcore_requirement.split("(")[-1].replace(")", "")
-    minor = req.split(".")[-1]
-    for index, c in enumerate(minor):
-        if not c.isdigit():
-            break
-    index = len(minor) if len(minor) == 1 else index
-    spec = SimpleSpec(req.replace(minor, minor[0:index]))
-    matches = spec.match(Version(pulpcore_version))
-    print(f"{plugin}-{plugin_version} requires: {pulpcore_requirement} is compatible: {matches}")
+        plugin_version = response.json()["info"]["version"]
+        plugin_requirements = response.json()["info"]["requires_dist"]
+        pulpcore_requirement = [r for r in plugin_requirements if "pulpcore" in r][0]
+        req = pulpcore_requirement.split("(")[-1].replace(")", "")
+        minor = req.split(".")[-1]
+        for index, c in enumerate(minor):
+            if not c.isdigit():
+                break
+        index = len(minor) if len(minor) == 1 else index
+        spec = SimpleSpec(req.replace(minor, minor[0:index]))
+        matches = spec.match(Version(pulpcore_version))
+        if matches:
+            if not shown:
+                print(f"\nCompatible with pulpcore-{pulpcore_version}")
+                shown = True
+            print(f" -> {plugin}-{plugin_version} requirement: {pulpcore_requirement}")
+            to_remove.append(plugin)
+
